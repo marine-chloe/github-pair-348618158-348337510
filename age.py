765877e1fa530ddc458b036adef87de2c5c8fe58
unknown <348618158@TDSB.ON.CA>
2022-04-14 13:34:04 -0400

    i love victoria

---
+++ age.py
@@ -0,0 +1,2 @@
+age = str(input("how old are you"))
+print ("you are ", age, "year old")
--- hungry.py
+++ hungry.py
@@ -3,4 +3,4 @@ if hungry == "yes":
   print("eat a burger")
   print("drink some milktea")
 else:
-  print("do your homework")
+  print("eat")
